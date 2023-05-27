2d3bc2ff852f27045c069334c405b288adc92511
minzzn <188min.kk@gmail.com>
2023-05-28 03:04:47 +0900

    0527

--- front/src/minju/Todo.jsx
+++ front/src/minju/Todo.jsx
@@ -5,16 +5,48 @@ function Todo() {
   return (
     <div className ="TodoBack">
       <div className ="Todobox">
-        <div className ="Headerbox"></div>
+        <div className ="Headerbox">
+          <p className = "Header">할 일 목록</p>
+        </div>
         <div className ="Todocontainer">
           <div className ="Listbox">
-            <div className ="Itembox"></div>
-            <div className ="Itembox"></div>
-            <div className ="Itembox"></div>
-            <div className ="Itembox"></div>
-            <div className ="Itembox"></div>
+            <div className ="Itembox">
+              <div className='Item'>
+                <p className="Inputtext">할 일을 입력하세요.</p>
+              </div>
+            </div>
+            <div className ="Itembox">
+              <div className='Item'>
+                <div className="Checkbox">
+                  <img src="asset/CheckMark.png" alt="CheckMark"/>
+                </div>
+              </div>
+            </div>
+            <div className ="Itembox">
+              <div className='Item'>
+                <div className="Checkbox"></div>
+              </div>
+            </div>
+            <div className ="Itembox">
+              <div className='Item'>
+                <div className="Checkbox"></div>
+              </div>
+            </div>
+            <div className ="Itembox">
+              <div className='Item'>
+                <div className="Checkbox"></div>
+              </div>
+            </div>
+            <div className ="Itembox">
+              <div className='Item'>
+                <div className="Checkbox"></div>
+              </div>
+            </div>
           </div>
-          <div className ="editbox"></div>
+          <div className ="Editbox">
+            <div className="Editbutton"></div>
+            <div className="Editbutton"></div>
+          </div>
         </div>
       </div>
     </div>
