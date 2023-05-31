50f1870f598a8e861973f62d4a4109020f0db1c3
minzzn <188min.kk@gmail.com>
2023-06-01 03:06:05 +0900

    input item까지

--- front/src/minju/Todo.jsx
+++ front/src/minju/Todo.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import './Todo.css';
+import {useState} from 'react';
 
 function Todo() {
+  const [toDo,setTodo] = useState("");
+  const [toDos,setTodos] = useState([]);
+  const onSubmit = (e) => {
+      e.preventDefault();
+      if(toDo === ""){
+          return
+      }
+      setTodos((currentArray) =>[toDo, ...currentArray])
+      setTodo("")
+  } 
+  const onChange = (e) => {
+      setTodo(e.target.value)
+  }
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') { //입력된 키값 검사
+      onSubmit(e);
+    }
+  }
   return (
     <div className ="TodoBack">
       <div className ="Todobox">
@@ -14,39 +33,23 @@ function Todo() {
           </div>
         </div>
         <div className ="Todocontainer">
-          <div className ="Listbox">
+          <div className ="ListTemplatebox">
             <div className ="Itembox">
-              <input className="InputItem" type="text" maxlength="200" placeholder="할 일을 입력하세요."/>
+              <form onSubmit={onSubmit}>
+                <input className="InputItem" type="text" maxLength="200" placeholder="할 일을 입력하세요." onChange={onChange} value={toDo}/>
+              </form>  
             </div>
-            <div className ="Itembox">
-              <div className='Item'>
-                <div className="Checkbox">
-                  <img className="CheckMark" src="asset/CheckMark.png" alt="CheckMark"/>
+            <div className ="Listbox">
+              {toDos.map((item)=>
+              <div className ="Itembox">
+                <div className='Item'>
+                  <div className="Checkbox">
+                    <img className="CheckMark" src="asset/CheckMark.png" alt="CheckMark"/>
+                  </div>
+                  <p className ="Buttontext">{item}</p>
                 </div>
-              </div>
-            </div>
-            <div className ="Itembox">
-              <div className='Item'>
-                <div className="Checkbox">
-                  <img className="CheckMark" src="asset/CheckMark.png" alt="CheckMark"/>
-                </div>
-              </div>
-            </div>
-            <div className ="Itembox">
-              <div className='Item'>
-                <div className="Checkbox"></div>
-              </div>
-            </div>
-            <div className ="Itembox">
-              <div className='Item'>
-                <div className="Checkbox"></div>
-              </div>
-            </div>
-            <div className ="Itembox">
-              <div className='Item'>
-                <div className="Checkbox"></div>
-              </div>
-            </div>
+              </div>)}
+            </div>  
           </div>
           <div className ="Editbox">
             <div className="Editbutton">
